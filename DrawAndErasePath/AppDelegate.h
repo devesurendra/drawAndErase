7e5cb683990bc9eeea3a50fbbbfaecbd711f5079
swamykottu <swamykottu@gmail.com>
2016-07-14 15:33:56 +0530

    Initial Commit

---
+++ DrawAndErasePath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawAndErasePath
+//
+//  Created by sagarsoft on 14/07/16.
+//  Copyright © 2016 sagarsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawAndErasePath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawAndErasePath
+//
+//  Created by sagarsoft on 14/07/16.
+//  Copyright © 2016 sagarsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
